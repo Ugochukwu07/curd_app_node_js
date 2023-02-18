01601d200be8f59dd7837bb9d32c504c19a8e6a3
Ekwueme Ugochukwu <ekwuemeugochukwu83@gmail.com>
2023-02-19 00:41:30 +0100

    setting up schema and controller medthods

---
+++ server/controller/controller.js
@@ -0,0 +1,21 @@
+var userdb = require('../model/model')
+
+//create and save new user
+exports.create = (res, req) => {
+
+}
+
+//retrive and return all usrers/retrive and return a single user
+exports.find = (res, req) => {
+    
+}
+
+//update a new identify user by user id
+exports.update = (res, req) => {
+    
+}
+
+//delete a user with specified user id
+exports.delete = (res, req) => {
+    
+}
