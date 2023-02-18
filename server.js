3f6fc1a7d76b8954844160153ee1e53f11aadc60
Ekwueme Ugochukwu <ekwuemeugochukwu83@gmail.com>
2023-02-18 17:56:51 +0100

    set up view engine and load assets

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const bodyparser = require('body-parser')
+// const path = require('path')
 
 const app = express()
 dotenv.config({path: 'config.env'})
 const PORT = process.env.PORT || 8080
+
+//log requests
+app.use(morgan('tiny'))
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended: true}))
+
+//set view engine
+app.set("view engine", 'ejs')
+// app.set("views", path.resolve(__dirname, "path-to-ejs-files"))   when ejs files are not in views
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
 app.get('/', (req, res) => {
     res.send('Curd Application')
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is Running on ${PORT}`)
+    console.log(`Server is Running on http://localhost:${PORT}`)
 })
